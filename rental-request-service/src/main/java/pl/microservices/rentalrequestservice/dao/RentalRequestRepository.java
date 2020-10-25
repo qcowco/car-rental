396a359ff4f9f682e7d6980f4d5c8abd4039aa99
qcowco <kamil.szuberski.g@gmail.com>
2020-10-25 16:46:52 +0100

    Add repository

---
+++ rental-request-service/src/main/java/pl/microservices/rentalrequestservice/dao/RentalRequestRepository.java
@@ -0,0 +1,9 @@
+package pl.microservices.rentalrequestservice.dao;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+import pl.microservices.rentalrequestservice.model.RentalRequest;
+
+@Repository
+public interface RentalRequestRepository extends PagingAndSortingRepository<RentalRequest, Long> {
+}
