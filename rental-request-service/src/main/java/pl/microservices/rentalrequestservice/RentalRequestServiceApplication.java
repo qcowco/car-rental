6e82e7d70f2f75fab239c69ee5e8452e54a2ff45
qcowco <kamil.szuberski.g@gmail.com>
2020-10-25 16:38:14 +0100

    Init rental request service

---
+++ rental-request-service/src/main/java/pl/microservices/rentalrequestservice/RentalRequestServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.microservices.rentalrequestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalRequestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalRequestServiceApplication.class, args);
+    }
+
+}
