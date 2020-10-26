c998309c9fc9c53ba2e628df6447be0b24f1ccfe
qcowco <kamil.szuberski.g@gmail.com>
2020-10-26 01:28:42 +0100

    Add client for rental-service

---
+++ rental-request-service/src/main/java/pl/microservices/rentalrequestservice/web/webclient/RentalClient.java
@@ -0,0 +1,19 @@
+package pl.microservices.rentalrequestservice.web.webclient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+import pl.microservices.rentalrequestservice.model.RentalRequest;
+
+import java.time.LocalDate;
+
+@FeignClient(value = "rental-service", path = "/api/v1/rentals")
+public interface RentalClient {
+    @GetMapping(path = "/{id}", params = { "start", "end" })
+    boolean isCarFree(@PathVariable Long id,
+                             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate start,
+                             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end);
+
+    @PostMapping
+    Long save(@RequestBody RentalRequest rental);
+}
