de2179905824be0743c7235ee11f0e99ceb2f1e9
qcowco <kamil.szuberski.g@gmail.com>
2020-10-26 13:11:49 +0100

    Add feign to module

--- car-service/src/main/java/pl/microservices/carservice/CarServiceApplication.java
+++ car-service/src/main/java/pl/microservices/carservice/CarServiceApplication.java
@@ -2,8 +2,10 @@ package pl.microservices.carservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CarServiceApplication {
 
     public static void main(String[] args) {
