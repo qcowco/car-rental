03dfae3966e1902236d0a7be76add79808003181
qcowco <kamil.szuberski.g@gmail.com>
2020-10-26 01:28:12 +0100

    Add client for car-service

---
+++ rental-request-service/src/main/java/pl/microservices/rentalrequestservice/web/webclient/CarClient.java
@@ -0,0 +1,12 @@
+package pl.microservices.rentalrequestservice.web.webclient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import pl.microservices.rentalrequestservice.web.webclient.dto.Car;
+
+@FeignClient(value = "car-service", path = "/api/v1/cars")
+public interface CarClient {
+    @GetMapping("/{id}")
+    Car findById(@PathVariable Long id);
+}
---
+++ rental-request-service/src/main/java/pl/microservices/rentalrequestservice/web/webclient/dto/Car.java
@@ -0,0 +1,8 @@
+package pl.microservices.rentalrequestservice.web.webclient.dto;
+
+import lombok.Data;
+
+@Data
+public class Car {
+    private Long id;
+}
