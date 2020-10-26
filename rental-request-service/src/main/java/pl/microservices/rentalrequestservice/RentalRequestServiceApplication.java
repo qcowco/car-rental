ff5328ce2c91a82935dd23589d25dec8bf42b0ef
qcowco <kamil.szuberski.g@gmail.com>
2020-10-26 01:26:44 +0100

    Add feign support

--- rental-request-service/src/main/java/pl/microservices/rentalrequestservice/RentalRequestServiceApplication.java
+++ rental-request-service/src/main/java/pl/microservices/rentalrequestservice/RentalRequestServiceApplication.java
@@ -2,8 +2,10 @@ package pl.microservices.rentalrequestservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class RentalRequestServiceApplication {
 
     public static void main(String[] args) {
