3550d34688053c8dec965dbade65a76d81b73985
qcowco <kamil.szuberski.g@gmail.com>
2020-10-28 00:28:53 +0100

    Inject Bearer tokens with Feign Interceptor

---
+++ car-service/src/main/java/pl/microservices/carservice/security/FeignTokenInterceptor.java
@@ -0,0 +1,26 @@
+package pl.microservices.carservice.security;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FeignTokenInterceptor implements RequestInterceptor {
+    @Override
+    public void apply(RequestTemplate template) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication instanceof KeycloakAuthenticationToken) {
+            KeycloakSecurityContext context = (KeycloakSecurityContext) authentication.getCredentials();
+
+            String jwtToken = context.getTokenString();
+
+            template.header("Authorization", String.format("Bearer %s", jwtToken));
+        }
+
+    }
+}
