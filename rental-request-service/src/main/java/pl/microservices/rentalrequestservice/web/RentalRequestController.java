d4b28bca79a60f234409cfd09d1ef6118b066c13
qcowco <kamil.szuberski.g@gmail.com>
2020-10-27 21:29:24 +0100

    Apply role-based authorization to endpoints

--- rental-request-service/src/main/java/pl/microservices/rentalrequestservice/web/RentalRequestController.java
+++ rental-request-service/src/main/java/pl/microservices/rentalrequestservice/web/RentalRequestController.java
@@ -1,16 +1,21 @@
 package pl.microservices.rentalrequestservice.web;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.*;
+import pl.microservices.rentalrequestservice.exception.UnauthorizedRequestException;
 import pl.microservices.rentalrequestservice.model.RentalRequest;
 import pl.microservices.rentalrequestservice.service.RentalRequestService;
 import pl.microservices.rentalrequestservice.web.dto.RequestEvaluation;
 import pl.microservices.rentalrequestservice.web.webclient.client.CarClient;
 import pl.microservices.rentalrequestservice.web.webclient.client.RentalClient;
 
+import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
@@ -27,31 +32,43 @@ public class RentalRequestController {
         this.rentalClient = rentalClient;
     }
 
-    @GetMapping
-    public Iterable<RentalRequest> findAll() {
-        return rentalRequestService.findAll();
-    }
-
     @GetMapping(params = { "page", "size" })
+    @RolesAllowed({ "moderator", "admin" })
     public List<RentalRequest> findAllPaged(@RequestParam int page, @RequestParam int size) {
         return rentalRequestService.findAll(page, size)
                 .getContent();
     }
 
     @GetMapping("/users/{requestedUsername}")
-    public Iterable<RentalRequest> findByUsername(@PathVariable String requestedUsername) {
+    @RolesAllowed({ "user", "moderator", "admin" })
+    @PreAuthorize("#username == authentication.principal.username || #roles == authentication.authorities")
+    public Iterable<RentalRequest> findByUsername(@PathVariable String requestedUsername, String username, Collection<GrantedAuthority> roles) {
+
+        if (isRegularUser(roles))
+            ownershipCheck(requestedUsername, username);
+
         return rentalRequestService.findByUsername(requestedUsername);
     }
 
     @GetMapping("/{id}")
-    public RentalRequest findById(@PathVariable Long id) {
-        return rentalRequestService.findById(id);
+    @RolesAllowed({ "user", "moderator", "admin" })
+    @PreAuthorize("#username == authentication.principal.username || #roles == authentication.authorities")
+    public RentalRequest findById(@PathVariable Long id, String username, Collection<GrantedAuthority> roles) {
+        RentalRequest rentalRequest = rentalRequestService.findById(id);
+
+        if (isRegularUser(roles))
+            ownershipCheck(rentalRequest.getUsername(), username);
+
+        return rentalRequest;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Long save(@Valid @RequestBody RentalRequest rentalRequest) {
+    @RolesAllowed({ "user", "moderator", "admin" })
+    @PreAuthorize("#username == authentication.principal.username")
+    public Long save(@Valid @RequestBody RentalRequest rentalRequest, String username) {
         rentalRequest.setId(null);
+        rentalRequest.setUsername(username);
 
         isCarRentable(rentalRequest);
 
@@ -61,13 +78,29 @@ public class RentalRequestController {
 
     @PutMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void put(@Valid @RequestBody RentalRequest rentalRequest) {
+    @RolesAllowed({ "user", "moderator", "admin" })
+    @PreAuthorize("#username == authentication.principal.username || #roles == authentication.authorities")
+    public void put(@Valid @RequestBody RentalRequest rentalRequest, String username, Collection<GrantedAuthority> roles) {
         if (Objects.isNull(rentalRequest.getId()))
             throw new IllegalArgumentException("Id need not be null while updating");
+
+        if (isRegularUser(roles))
+            ownershipCheck(rentalRequest.getUsername(), username);
 
         isCarRentable(rentalRequest);
 
         rentalRequestService.save(rentalRequest);
+    }
+
+    private boolean isRegularUser(Collection<GrantedAuthority> roles) {
+        return roles.stream()
+                .map(GrantedAuthority::getAuthority)
+                .noneMatch(role -> role.equals("moderator") || role.equals("admin"));
+    }
+
+    private void ownershipCheck(String requestedUsername, String username) {
+        if (!requestedUsername.equals(username))
+            throw new UnauthorizedRequestException("Action only permitted for resource owners");
     }
 
     private void isCarRentable(RentalRequest rentalRequest) {
@@ -92,6 +125,7 @@ public class RentalRequestController {
 
     @PostMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RolesAllowed({ "moderator", "admin" })
     public void evaluateRequest(@PathVariable Long id, @RequestBody RequestEvaluation requestEvaluation) {
         if (requestEvaluation.isApproved()) {
             RentalRequest rentalRequest = rentalRequestService.findById(id);
