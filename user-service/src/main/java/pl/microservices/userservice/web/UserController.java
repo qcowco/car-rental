6a6791aba1bf027f439c808a347fd27fa6dcc1b1
qcowco <kamil.szuberski.g@gmail.com>
2020-10-28 21:33:24 +0100

    Validate request entities

--- user-service/src/main/java/pl/microservices/userservice/web/UserController.java
+++ user-service/src/main/java/pl/microservices/userservice/web/UserController.java
@@ -6,12 +6,16 @@ import org.keycloak.admin.client.resource.UsersResource;
 import org.keycloak.authorization.client.AuthzClient;
 import org.keycloak.representations.AccessTokenResponse;
 import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import pl.microservices.userservice.model.LoginRequest;
 import pl.microservices.userservice.model.UserRequest;
 import pl.microservices.userservice.service.UserService;
+
+import javax.validation.Valid;
 
 @RestController
 public class UserController {
@@ -26,7 +30,8 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public void register(@RequestBody UserRequest userRequest) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public void register(@Valid @RequestBody UserRequest userRequest) {
         RealmResource realmResource = keycloak.realm("SpringBootKeycloak");
         UsersResource users = realmResource.users();
 
@@ -36,7 +41,7 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequest loginRequest) {
+    public String login(@Valid @RequestBody LoginRequest loginRequest) {
         AccessTokenResponse accessTokenResponse = authzClient.obtainAccessToken(
                 loginRequest.getUsername(),
                 loginRequest.getPassword()
