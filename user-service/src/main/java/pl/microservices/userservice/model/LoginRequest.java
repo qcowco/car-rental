42e5c1bd405e3be7a4187130ae95910897496035
qcowco <kamil.szuberski.g@gmail.com>
2020-10-28 21:32:25 +0100

    Apply validation constraints

--- user-service/src/main/java/pl/microservices/userservice/model/LoginRequest.java
+++ user-service/src/main/java/pl/microservices/userservice/model/LoginRequest.java
@@ -2,8 +2,15 @@ package pl.microservices.userservice.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 public class LoginRequest {
+    @Size(min = 5, max = 20)
+    @NotNull
     private String username;
+    @Size(min = 5, max = 20)
+    @NotNull
     private String password;
 }
--- user-service/src/main/java/pl/microservices/userservice/model/UserRequest.java
+++ user-service/src/main/java/pl/microservices/userservice/model/UserRequest.java
@@ -2,11 +2,26 @@ package pl.microservices.userservice.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Data
 public class UserRequest {
+    @Size(min = 5, max = 20)
+    @NotNull
     private String username;
+    @Size(min = 5, max = 20)
+    @NotNull
     private String password;
+    @Size(min = 2, max = 25)
+    @NotNull
     private String firstName;
+    @Size(min = 2, max = 25)
+    @NotNull
     private String lastName;
+    @Pattern(regexp = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+    @Size(min = 5, max = 25)
+    @NotNull
     private String email;
 }
